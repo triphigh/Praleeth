398794da9fa429ebfc33306a1cb4c05a0d244a62
triphigh <syed.hashim@g7cr.in>
2017-06-07 11:16:32 +0530

    Changes

---
+++ Artifact/File-upload/newfile.ps1
@@ -0,0 +1,30 @@
+﻿Function Get-RedirectedUrl
+{
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]$URL
+    )
+ 
+    $request = [System.Net.WebRequest]::Create($url)
+    $request.AllowAutoRedirect=$false
+    $response=$request.GetResponse()
+ 
+    If ($response.StatusCode -eq "Found")
+    {
+       $response.GetResponseHeader("Location")
+    }
+}
+
+$url = 'https://createanaccount1298-my.sharepoint.com/personal/trip_createanaccount1298_onmicrosoft_com/_layouts/15/guestaccess.aspx?docid=1f9db9d4d11744c20aa2326709c0eea12&authkey=ASeRRa_r-8oySMZL2HSZheI'
+$codeSetupUrl = Get-RedirectedUrl -URL $url
+
+$uploadfilecodeSetup = "${env:Temp}\AZURE ACTIVE DIRECTORY B2B COLLABORATION.docx"
+
+try
+{
+    (New-Object System.Net.WebClient).DownloadFile($codeSetupUrl, $uploadfilecodeSetup)
+}
+catch
+{
+    Write-Error "Failed to download a file"
+}
